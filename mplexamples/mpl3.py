3f43ade045a19f45475ac933f607d9094c0751e8
Megan Squire <megansquire@gmail.com>
2017-06-05 14:13:23 -0400

    Update mpl3.py

--- mplexamples/mpl3.py
+++ mplexamples/mpl3.py
@@ -7,14 +7,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data = np.genfromtxt('codeplexCreateDates.csv', delimiter=',', skip_header=0,
-                     skip_footer=0, names=['x', 'y', 'z'])
+                     skip_footer=0, names=['x', 'xlabels', 'y'])
 
+# remove points that are too high
 check = (data['z'] < 1000)
 data = data[check]
 
-plt.plot(data['x'], data['z'], color='r', label='the data')
+plt.plot(data['x'], data['y'], color='r', label='the data')
+
+# add customizations
 plt.title('Number of monthly new project registrations on CodePlex, 2006-2017')
 plt.xlabel('Month Number')
 plt.ylabel('Count of new projects')
-
 plt.figtext(0.85, 0.75, 'potential spam', ha='right', va='bottom')
